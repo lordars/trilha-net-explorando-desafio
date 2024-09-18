23eccfaefdecdf60b0f4aedb033450988055d216
Adrián Ricardo Iazbeck Scalia <adrian.r.scalia@gmail.com>
2024-09-17 23:41:42 -0300

    Resolução do desafio com mensagem e tratando errors

---
+++ Models/HandleErrors.cs
@@ -0,0 +1,22 @@
+﻿namespace DesafioProjetoHospedagem.Models;
+using System;
+
+
+public class HandleErrors : Exception
+{
+
+
+    public string OvercapacityException(int qtdPessoa, int capacidade)
+    {
+        string messageError = $"A capacidad do quarto foi sobrepassada \r\n  O quarto só comporta {capacidade} \r\n  Mas  a reserva é para {qtdPessoa}.  \r\n Volte a refazer sua reserva";
+            return messageError;
+    }
+
+
+
+      public string DescontFailException(decimal valor, decimal desconto)
+    {
+        string messageError = $"As informações passadas apresentarão erros \r\n  O Valor da recerba é {valor} tipo decimal \r\n O desconto: {desconto} tipo decimal.  \r\n Comprobe que os valores são corretos";
+            return messageError;
+    }
+}
--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -1,11 +1,16 @@
 namespace DesafioProjetoHospedagem.Models
 {
+
+
     public class Reserva
     {
+
         public List<Pessoa> Hospedes { get; set; }
         public Suite Suite { get; set; }
         public int DiasReservados { get; set; }
-
+        public int DiasParaDesconto = 10;
+        public int descontoPromo = 10;
+        bool comDesconto = false;
         public Reserva() { }
 
         public Reserva(int diasReservados)
@@ -16,15 +21,23 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            int qtdPessoa = hospedes.Count;
+
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (Suite.Capacidade >= qtdPessoa)
             {
                 Hospedes = hospedes;
             }
             else
             {
+                HandleErrors error = new HandleErrors();
+
+                Console.WriteLine(error.OvercapacityException(Suite.Capacidade, qtdPessoa));
+
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+
             }
         }
 
@@ -37,24 +50,57 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+
+            return Hospedes.Count;
         }
 
-        public decimal CalcularValorDiaria()
+        public string CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
+            comDesconto = false;
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
+            valor = CalcularDesconto(valor, descontoPromo);
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            
+            return comDesconto? $"Tuviste un desconto Genial de {descontoPromo} el valor de tua Suite é : {valor}" : $"O valor da Suite é: {valor}, mas temos um desconto especial se reserva por 10 dias o mas, não perca!";
+        }
+
+
+        public decimal CalcularDesconto(decimal valor, decimal desconto)
+        {
+
+            decimal descontoTotal = 0;
+
+            try
             {
-                valor = 0;
+                if (DiasReservados >= DiasParaDesconto)
+                {
+                    descontoTotal = valor - (valor * desconto / 100);
+                    comDesconto = true;
+                }
+                else
+                {
+                    return valor;
+                }
+
+
+                return descontoTotal;
+            }
+            catch (System.Exception)
+            {
+                HandleErrors error = new HandleErrors();
+                Console.WriteLine(error.DescontFailException(valor, desconto));
+                return valor;
             }
 
-            return valor;
+
         }
     }
 }
